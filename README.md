f084c81f7895da7a8cde790896d4318bfdf1da77
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-07 22:22:28 +0200

    Make methods

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # merge-sort
+
+A two version method, one non-recursive and one recursive, to return a specific Fibonacci number
+
+Then, my take on the merge sort algorithm made in Ruby
---
+++ merge_sort.rb
@@ -0,0 +1,76 @@
+def fibs(n)
+    i = 0
+    sequence = [0, 1]
+    while i < n do        
+        sequence.push(sequence[sequence.length - 2] + sequence[sequence.length - 1])
+        i += 1
+    end
+    puts sequence.join(', ')
+end
+
+def fibs_rec(sequence, n)
+    if n > 0
+        sequence.push(sequence[sequence.length - 2] + sequence[sequence.length - 1])
+        fibs_rec(sequence, n - 1)
+    end
+    sequence
+end
+
+def merge_sort(array)
+    if array.length == 1
+        return array
+    elsif array.length == 2
+        left_side = array.slice(0, array.length / 2)
+        right_side = array.slice(array.length / 2, array.length - 1)
+        temp = []
+
+        until left_side.empty? == true && right_side.empty? == true
+            if left_side.empty? == true && right_side.empty? == false
+                temp.push(right_side[0])
+                right_side.shift
+            elsif right_side.empty? == true && left_side.empty? == false
+                temp.push(left_side[0])
+                left_side.shift
+            elsif left_side[0] < right_side[0]
+                temp.push(left_side[0])
+                left_side.shift
+            elsif left_side[0] > right_side[0]
+                temp.push(right_side[0])
+                right_side.shift
+            end
+        end
+        return temp
+    else
+        left_side = array.slice(0, array.length / 2)
+        right_side = array.slice(array.length / 2, array.length - 1)
+        left_side = merge_sort(left_side)
+        right_side = merge_sort(right_side)
+
+        temp = []
+
+        until left_side.empty? == true && right_side.empty? == true
+            if left_side.empty? == true && right_side.empty? == false
+                temp.push(right_side[0])
+                right_side.shift
+            elsif right_side.empty? == true && left_side.empty? == false
+                temp.push(left_side[0])
+                left_side.shift
+            elsif left_side[0] < right_side[0]
+                temp.push(left_side[0])
+                left_side.shift
+            elsif left_side[0] > right_side[0]
+                temp.push(right_side[0])
+                right_side.shift
+            end             
+        end
+        return temp
+    end
+
+    array = left_side + right_side
+end
+
+array = [1, 32, 54, 34, 67, 89, 33, 5, 43, 31, 543, 234, 71]
+
+array = merge_sort(array).join(", ")
+
+puts array
